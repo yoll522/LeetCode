aad29e8dcf99f11ab2b4324f59677ec9a2abda88
yoll522 <yoll5222@gmail.com>
2017-06-18 11:29:14 +0800

    Add files via upload

--- 338. Counting Bits/src/Solution.java
+++ 338. Counting Bits/src/Solution.java
@@ -2,8 +2,6 @@
 public class Solution {
     public int[] countBits(int num) {
         int []bits = new int[num+1];
-    	//For num = 5 you should return [0,1,1,2,1,2,2,3,1,2,2,3,2,3,3,4].
-    	
         for(int i = 1 ; i <= num ; ++i){
         	bits[i]= bits[i / 2] + ( i & 1);
         }
